439f1c358a9fad712be2a4fd1a5e139b9e83e959
Helsldaaeh <Coldberserk2006@gmail.com>
2024-01-18 19:20:56 +0300

    Update Form1.cs

--- Code Calendar/Form1.cs
+++ Code Calendar/Form1.cs
@@ -29,25 +29,6 @@ namespace Code_Calendar
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
-            foreach (DayComponent dayComponent in DayComponents)
-            {
-                dayComponent.Select() 
-            }
-            DayComponent newTextBox = new DayComponent();
-            DayComponent lastOldTextBox = DayComponents.LastOrDefault();
-                if (lastOldTextBox == null)
-                {
-                    newTextBox.Location = new Point(30, 50);
-                    newTextBox.Text = "Hello World!";
-                }
-                else
-                {
-                    newTextBox.Location = new Point(lastOldTextBox.Location.X, lastOldTextBox.Location.Y + 30);
-                    newTextBox.Text = $"Hello World! ({DayComponents.Count})";
-                }
-                DayComponents.Add(newTextBox);
-            this.Controls.Add(newTextBox);
         }
 
         private void textBox6_TextChanged(object sender, EventArgs e)
